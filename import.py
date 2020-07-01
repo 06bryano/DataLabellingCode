d850844c5f2d6a2b76bc91e5134eeb59437570ee
Oscar Bryan <ozzybryan@gmail.com>
2020-07-01 13:42:59 +0100

    labelled an entire small section

--- import.py
+++ import.py
@@ -11,17 +11,17 @@ from tkinter import *
 #e.pack()
 #e.focus_set()
 class GUI:
-    def __init__(self, filename):
+    def __init__(self, filename, labelsTXTfile):
         self.ObjClassList = ["105mm_Shell","150mm_Shell","KC50","KC250","KC500","Drum","Can","Crate","Grenade","Debris_(man_made)","Natural"]
         self.CertaintyList = ["Certain","Plausable","low_certainty","very uncertain"]
         self.obj = []
         self.filename = filename
+        self.labelsTXTfile = labelsTXTfile
     def ObjChoiceCallback(self,window, ObjClass):
         self.obj.append(ObjClass)
-    def OKCallback(self,window,corners):
+    def OKCallback(self,window,corners,labelsTXTfile):
         print("save", self.obj, len(self.obj))
-        f = open("demofile2.txt", "a") # "a" create a new file if the specified file doesnt exists        
-        
+        f = open(self.labelsTXTfile , "a") # "a" create a new file if the specified file doesnt exists        
         f.write(self.filename)
         f.write("~")
         f.write(str(self.obj))
@@ -92,7 +92,7 @@ def toggle_selector(event):
         
 
 class data:
-    def __init__(self,d):
+    def __init__(self,d, labelsTXTfile):
         self.rawData = d
         #rawData (Dictionary) items x_pos,y_pos,z_pos,img_out1
         self.intensities = 10*np.log10(self.rawData['img_out1'] / 10)  # in dB....this needs talking through with Alan!!!
@@ -102,7 +102,7 @@ class data:
         self.Rangex = [self.xpos[0] , self.xpos[-1]]
         self.Rangey = [self.ypos[0] , self.ypos[-1]]
         self.targetClasses = ["Munition","Debris (man made)","Natural","unknown"]
-   
+        self.labelsTXTfile = labelsTXTfile
 
     def display_segment(self):
 
@@ -119,12 +119,19 @@ class data:
         toggle_selector.RS = RectangleSelector(self.ax,  line_select_callback,drawtype='box', useblit=True,button=[1, 3], minspanx=5, minspany=5,spancoords='pixels',interactive=True)
         plt.connect('key_press_event', toggle_selector)
         plt.show()
+        
+    def displayLabels(self):
+        #read txt file
+        f.open(SASdata.labelsTXTfile)
+        lines = f.readlines()
 
 filename = "sasi-20150413-181203-vrak_13c-2-SLH90-BP-000_simppackage.mat"
 #filename = "sasi-20150413-181203-vrak_13c-2-PLH90-BP-000_simppackage.mat"
 d = loadmat(r'../DataLabelled/' + filename)
 
-SASdata = data(d)
-myGUI = GUI(filename)
+labelsTXTfile = "demolabelfile.txt"
+
+SASdata = data(d, labelsTXTfile)
+myGUI = GUI(filename, labelsTXTfile)
 
 SASdata.display_segment()
